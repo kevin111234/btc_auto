161e9460bb23910a402387ad1e1bd0662b0b3830
woghd111234 <jaehong243587@gmail.com>
2024-11-13 20:12:40 +0900

    slack 메시지 계속 전송되는 오류 삭제

--- BTC_Trader.py
+++ BTC_Trader.py
@@ -217,7 +217,6 @@ def main():
             # 현재 구매한 자산이 없을때 자산 데이터 조회 후 구매한도 재설정
             if len(rsi_check) == 0:
                 asset_info = get_asset_info(upbit)
-                send_asset_info(asset_info)
                 if asset_info is None:
                     send_slack_message("자산 정보 조회 실패, 10초 대기 후 다시 시도합니다...")
                     time.sleep(10)
