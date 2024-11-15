dc64e74c3c032813afdd9c1388ca2625aafe61f3
woghd111234 <jaehong243587@gmail.com>
2024-11-15 14:52:41 +0900

    환경변수 설정이 잘 되었는지 확인하는 함수 작성

--- auto_trader/config.py
+++ auto_trader/config.py
@@ -17,6 +17,22 @@ class Config:
         self.slack_asset_channel = os.getenv("SLACK_ASSET_CHANNEL")
         # coin_ticker
         self.coin_ticker = os.getenv("COIN_TICKER").split(" ")
+        # 테스트 여부
+        self.verify()
+
+    def verify(self):
+        if not self.upbit_access_key or not self.upbit_secret_key:
+            raise ValueError("UPBIT_ACCESS_KEY 또는 UPBIT_SECRET_KEY가 설정되지 않았습니다")
+        if not self.slack_api_token:
+            raise ValueError("SLACK_API_TOKEN이 설정되지 않았습니다")
+        if not self.slack_trade_channel:
+            raise ValueError("SLACK_TRADE_CHANNEL이 설정되지 않았습니다") 
+        if not self.slack_error_channel:
+            raise ValueError("SLACK_ERROR_CHANNEL이 설정되지 않았습니다")
+        if not self.slack_asset_channel:
+            raise ValueError("SLACK_ASSET_CHANNEL이 설정되지 않았습니다")
+        if not self.coin_ticker:
+            raise ValueError("COIN_TICKER가 설정되지 않았습니다")
 
 if __name__ == "__main__":
     print("config 테스트")
