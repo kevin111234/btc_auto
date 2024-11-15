8acc5c02718977e5e791f8c1c831863750257413
woghd111234 <jaehong243587@gmail.com>
2024-11-15 14:59:39 +0900

    slack 메시지 전송 함수 작성

--- auto_trader/api.py
+++ auto_trader/api.py
@@ -7,3 +7,12 @@ class API:
         self.config = Config()
         self.upbit = pyupbit.Upbit(self.config.upbit_access_key, self.config.upbit_secret_key)
         self.slack = slack.WebClient(token=self.config.slack_api_token)
+    def send_slack_message(self, channel_id, message):
+        try:
+            self.slack.chat_postMessage(channel=channel_id, text=message)
+        except Exception as e:
+            print(f"Error sending message: {e}")
+
+if __name__ == "__main__":
+    print("api 테스트")
+    api = API()
