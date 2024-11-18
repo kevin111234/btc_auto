30a1464b863dabe23272e91dad34695e7de9db53
woghd111234 <jaehong243587@gmail.com>
2024-11-18 10:32:16 +0900

    Trader 클래스 다시 작성

---
+++ auto_trader/trader.py
@@ -0,0 +1,23 @@
+import api
+import notifier
+import indicator
+
+class Trader:
+    def __init__(self, currency):
+        self.currency = currency
+        self.api = api.API()
+        self.notifier = notifier.Notifier()
+        self.indicator = indicator.Indicator(currency)
+
+    def signal_check(self, asset_info):
+        data = self.api.get_ohlcv(self.currency, interval='minute5')
+        rsi, previous_rsi = self.indicator.calculate_rsi(data)
+
+        buy_signal = (rsi <= 35 and previous_rsi <= rsi)
+        sell_signal = (rsi >= 65 and previous_rsi >= rsi and
+                      asset_info['coin_info'][self.currency]['profit_rate'] >= 0.5)
+        
+        new_rsi = self.indicator.get_new_rsi(data)
+
+        return buy_signal, sell_signal, new_rsi, rsi
+
