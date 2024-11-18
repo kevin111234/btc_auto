e9fe243902ee221c13a7dc50b0d5996b0d364e47
woghd111234 <jaehong243587@gmail.com>
2024-11-18 11:32:21 +0900

    파일 의존성 수정

--- auto_trader/api.py
+++ auto_trader/api.py
@@ -1,12 +1,12 @@
 import pyupbit
-import slack
+from slack_sdk.web import WebClient
 from config import Config
 
 class API:
     def __init__(self):
         self.config = Config()
         self.upbit = pyupbit.Upbit(self.config.upbit_access_key, self.config.upbit_secret_key)
-        self.slack = slack.WebClient(token=self.config.slack_api_token)
+        self.slack = WebClient(token=self.config.slack_api_token)
 
     def send_slack_message(self, channel_id, message):
         try:
